fbed8de1fcf8bc42edb654b2286ec05f2dce4094
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2024-11-05 01:04:47 +0300

    refactored manifest and activity files

--- app/src/main/java/com/example/noteapplication/MainActivity.kt
+++ app/src/main/java/com/example/noteapplication/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.noteapplication.ui.theme.NoteApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
